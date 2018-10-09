f312756c017cddccd90ef3960552dd7f80e48e5e
Jordan Owiesny <jordanowiesny@quickenloans.com>
2018-10-09 11:17:31 -0400

    Lab3 Complete

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -8,8 +8,37 @@ namespace Lab3
 {
     class Program
     {
+        static string choice()
+            {
+            Console.Write("Enter a Number: ");
+            int num = int.Parse(Console.ReadLine());
+            string s = "";
+
+            if (num % 2 == 0 && num > 60)
+                s = num.ToString() + ", and Even.";
+            else if (num % 2 != 0)
+                s = num.ToString() + ", and Odd.";
+            else if
+                (num > 25 && num < 61)
+                s = "Even.";
+            else if (num % 2 == 0 && (num <= 25 && num >= 2))
+                s = "Even and less then 25.";
+            
+            return s;
+            }
         static void Main(string[] args)
         {
+            string test = "y";
+            while (test == "y")
+            {
+                Console.WriteLine(choice());
+                Console.Write("\nAgain (y/n)?: ");
+                test = Console.ReadLine();
+                if (test == "y")
+                    Console.WriteLine();
+            }
+
+            Console.WriteLine("End.");
         }
     }
 }
