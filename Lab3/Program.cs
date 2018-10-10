4dd91e0e562b0998c75fe1bd3495c3fa54e43173
Jordan Owiesny <jordanowiesny@quickenloans.com>
2018-10-10 16:59:17 -0400

    Cleaned up for submission

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -3,46 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Lab3
 {
     class Program
     {
-        static string choice(int num)
-            {
-            string s = "";
-
+          static string choice(int num)
+          {
             if (num % 2 == 0 && num > 60)
-                s = num.ToString() + ", and Even.";
+                return num.ToString() + ", and Even.";
             else if (num % 2 != 0)
-                s = num.ToString() + ", and Odd.";
+                return num.ToString() + ", and Odd.";
             else if
                 (num > 25 && num < 61)
-                s = "Even.";
+                return "Even.";
             else if (num % 2 == 0 && (num <= 25 && num >= 2))
-                s = "Even and less then 25.";
-            
-            return s;
-            }
+                return "Even and less then 25.";
+            return "ERROR.";
+          }
+
         static void Main(string[] args)
         {
-            string test = "y";
-            while (test == "y")
+            string test;
+            int num;
+            while (true)
             {
-                Console.Write("Enter a Number: ");
-                int num = int.Parse(Console.ReadLine());
+                try
+                {
+                    Console.Write("Enter a Number: ");
+                    num = int.Parse(Console.ReadLine());
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("SHIT CODE");
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
                 if (num < 1 || num > 100)
                 {
                     Console.WriteLine("Invalid Entry (1-100)");
                     continue;
                 }
                 Console.WriteLine(choice(num));
-                Console.Write("\nAgain (y/n)?: ");
+                Console.Write("\nPress 'y' to go again, any key to exit?: ");
                 test = Console.ReadLine();
                 if (test == "y")
                     Console.WriteLine();
+                else
+                    break;
             }
-
             Console.WriteLine("End.");
         }
     }
